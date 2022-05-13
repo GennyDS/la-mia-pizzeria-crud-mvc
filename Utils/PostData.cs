6646e7515dfb3e47e0d937146055455ac52e33c5
Gennaro <gennarinodi02@gmail.com>
2022-05-13 16:46:27 +0200

    Add project files.

---
+++ Utils/PostData.cs
@@ -0,0 +1,32 @@
+﻿using NetCore_01.Models;
+
+namespace NetCore_01.Utils
+{
+    public static class PostData
+    {
+
+        private static List<Post> posts;
+
+        public static List<Post> GetPosts()
+        {
+            if(PostData.posts != null)
+            {
+                return PostData.posts;
+            }
+
+            List<Post> nuovaListaPosts = new List<Post>();
+
+            for(int i=0; i<2; i++)
+            {
+                Post post = new Post(i, "Nome Pizza: " + i, " Descrizione della pizza.","Immagine" + i + "/800/500");
+                nuovaListaPosts.Add(post);
+            }
+
+            PostData.posts = nuovaListaPosts;
+
+            return PostData.posts;
+
+        }
+
+    }
+}
